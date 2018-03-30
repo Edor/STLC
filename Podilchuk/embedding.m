9a3bb2bafe11808708c24e9f5ac19bff582640b0
Edor <em.dortenzi@gmail.com>
2018-03-30 13:30:18 +0200

    Directories refactory

--- Podilchuk/embedding.m
+++ Podilchuk/embedding.m
@@ -1,4 +1,4 @@
-function [] = embedding(I, w)
+function [] = embedding(I, alpha, w)
 if (size(I,1)*size(I,2)/4) ~= size(w,1)
     error('Errore: immagine e marchio di dimensioni incompatibili')
 end
@@ -8,8 +8,8 @@ counter = 1;
 for i = 1:N
     for j = 1:N
         temp = dct2(C{i,j});
-        temp(1,2) = w(counter);
-        temp(2,1) = w(counter+1);
+        temp(1,2) = temp(1,2) + alpha*w(counter);
+        temp(2,1) = temp(2,1) + alpha*w(counter+1);
         counter = counter + 2;
         C{i,j} = idct2(temp);
     end
